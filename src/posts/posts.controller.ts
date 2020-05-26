546b7e999dbd49c09e82f54d7e72f7d0912ff667
Taha Farahani <iotahacodes@gmail.com>
2020-05-26 22:17:01 +0430

    some basics

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get()
+  findAll(): string {
+    return 'All the posts';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Post ${id}`;
+  }
+
+  @Post()
+  createPost(@Body() createPostDto: CreatePostDto): string {
+    return `Name: ${createPostDto.title} Description: ${createPostDto.desc} Author: ${createPostDto.author}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updatePostDto: CreatePostDto, @Param('id') id): string {
+    return `Update ${id} - Name: ${updatePostDto.title}`;
+  }
+}
